ccff1588324debd88e8811217bca9a20301219c5
Joao Pedro Rocha <joaopedrorocha2211@gmail.com>
2026-04-21 21:26:12 -0300

    getters/setters, pacote e encapsulamento

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,16 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        Filme meuFilme = new Filme(); // OBJETO CRIADO NA MEMÓRIA ATRAVÉS DA CLASSE FILME
+        meuFilme.setNome(" O poderoso chefão "); //Quando eu altero para set, o nome do filme fica dentro dos (), ai não preciso usar mais o =
+        meuFilme.setAnoDeLancamento( 1970);
+        meuFilme.setDuracaoEmMinutos(180);
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.obterMedia());
     }
 }
